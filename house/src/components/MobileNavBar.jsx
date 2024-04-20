28ea6d09c0a1fced29dd58845775e7890d1f596f
thkhomie <prajal.gurung17@gmail.com>
2024-04-20 21:05:34 +0545

    change

--- house/src/components/MobileNavBar.jsx
+++ house/src/components/MobileNavBar.jsx
@@ -1,15 +1,17 @@
 import Logo from '../assets/Logo.png'
 
-export default function MobileNavBar({isMenuOpen, setIsMenuOpen}){
+export default function MobileNavBar({ isMenuOpen, setIsMenuOpen }) {
 
-    const handleSelect = (sectionId) =>{
-        if(isMenuOpen) setIsMenuOpen(false);
-        document.getElementById(sectionId).scrollIntoView({behavior:'smooth'})
+    const handleSelect = (sectionId) => {
+        if (isMenuOpen) setIsMenuOpen(false);
+        document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' })
     }
-    return(
+    return (
         <div className="w-screen fixed top-0 z-20">
             <div className="w-1/2 h-screen flex flex-col p-8 bg-background">
-                <img className="w-16 object-contain mb-8 rounded-t-full" src={Logo} alt="company-logo" />
+                <a href="/">
+                    <img className="w-16 object-contain mb-8 rounded-t-full" src={Logo} alt="company-logo" />
+                </a>
 
                 <ul>
                     <li className="mb-5">
@@ -31,9 +33,9 @@ export default function MobileNavBar({isMenuOpen, setIsMenuOpen}){
 
                 <button className="h-10 bg-primary text-white text-sm px-6 rounded hover:bg-blue-200 hover:text-primary md:block">Contact Us</button>
 
-                <div 
-                className="w-screen h-screen bg-overlayBg fixed top-0 -z-10" 
-                onClick={() => {setIsMenuOpen(false)}} />
+                <div
+                    className="w-screen h-screen bg-overlayBg fixed top-0 -z-10"
+                    onClick={() => { setIsMenuOpen(false) }} />
             </div>
         </div>
     )
--- house/src/components/NavBar.jsx
+++ house/src/components/NavBar.jsx
@@ -13,36 +13,39 @@ export default function NavBar() {
             {isMenuOpen && <MobileNavBar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />}
 
             <div className='bg-background sticky top-0 z-10'>
-            
-            <nav className='max-w-screen-xl mx-auto py-4 px-6'>
-                <div className="flex justify-between items-center">
-                    <img src={Logo} alt="company-logo" className='rounded-full size-12' />
 
-                    <ul className="list-none hidden md:flex md:gap-14">     {/*mobile md*/}
-                        <li>
-                            <Link to='home' smooth spy offset={-80} className="menu-item">Home</Link>
-                        </li>
-                        <li>
-                            <Link to='services' smooth spy offset={-80} className="menu-item">Services</Link>
-                        </li>
-                        <li>
-                            <Link to='our-work' smooth spy offset={-80} className="menu-item">Our Work</Link>
-                        </li>
-                        <li>
-                            <Link to='testimonals' smooth spy offset={-80} className="menu-item">Testimonals</Link>
-                        </li>
-                    </ul>
-
-                    <button className='hidden h-10 bg-primary text-white text-sm px-6 rounded hover:bg-blue-200 hover:text-primary md:block'>Contact Us</button>
-
-                    <button onClick={() => {setIsMenuOpen(true)}} className='w-11 h-11 bg-blue-100 text-2xl text-primary flex items-center justify-center rounded md:hidden'>
-                        {isMenuOpen ? <RiCloseFill/> :<RiMenu4Line />}
-                    </button>
-                </div>
+                <nav className='max-w-screen-xl mx-auto py-4 px-6'>
+                    <div className="flex justify-between items-center">
+                        <a href="/">
+                            <img src={Logo} alt="company-logo" className='rounded-full size-12' />
+                        </a>
 
 
-            </nav>
-        </div>
+                        <ul className="list-none hidden md:flex md:gap-14">     {/*mobile md*/}
+                            <li>
+                                <Link to='home' smooth spy offset={-80} className="menu-item">Home</Link>
+                            </li>
+                            <li>
+                                <Link to='services' smooth spy offset={-80} className="menu-item">Services</Link>
+                            </li>
+                            <li>
+                                <Link to='our-work' smooth spy offset={-80} className="menu-item">Our Work</Link>
+                            </li>
+                            <li>
+                                <Link to='testimonals' smooth spy offset={-80} className="menu-item">Testimonals</Link>
+                            </li>
+                        </ul>
+
+                        <button className='hidden h-10 bg-primary text-white text-sm px-6 rounded hover:bg-blue-200 hover:text-primary md:block'>Contact Us</button>
+
+                        <button onClick={() => { setIsMenuOpen(true) }} className='w-11 h-11 bg-blue-100 text-2xl text-primary flex items-center justify-center rounded md:hidden'>
+                            {isMenuOpen ? <RiCloseFill /> : <RiMenu4Line />}
+                        </button>
+                    </div>
+
+
+                </nav>
+            </div>
         </>
     )
 }
